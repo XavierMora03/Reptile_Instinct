819561773c33e4fa2721bc1dd55f3940ec244cb2
xavier <diego.mora8157@alumnos.udg.mx>
2023-02-07 22:05:52 -0500

    cree un nuevo modulo de node

--- database-queries/main.js
+++ database-queries/main.js
@@ -13,11 +13,15 @@ con.connect(function(err) {
     "INSERT INTO store.reptiles(name,regularPrice,price,age,description,genre) VALUES('piton bobola','6000','4500','Cria','esta bn','H')";
   con.query(myquery, (err, res) => {
     if (err) throw err;
+    console.log(res);
   });
 
   con.query("SELECT * FROM store.reptiles", (err, res) => {
     if (err) throw err;
     console.log("este es el resultado epico");
     console.log(res);
+    console.log(typeof res);
+
+    console.log("este es el numero 3", res[2].id);
   });
 });
