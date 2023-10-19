9609d19d862016552ab593a1a3208845e302000c
XavierMora03 <diego.mora5781@alumnos.udg.mx>
2023-10-19 13:52:08 -0600

    trying to make cors work with AWS

--- api/retriveReptiles.js
+++ api/retriveReptiles.js
@@ -1,5 +1,6 @@
 const Http = new XMLHttpRequest();
-const url = 'http://localhost:8080/reptiles';
+const url = 'http://dh89u63wwk.execute-api.us-east-2.amazonaws.com';
+// const url = 'http://localhost:8080/reptiles';
 Http.open("GET", url);
 Http.send();
 
